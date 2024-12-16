d9272e68c54feded08232f80cf6c5365985be55d
aaronke16 <124176003+aaronke16@users.noreply.github.com>
2024-12-15 20:25:46 -0500

    adding the /pages path

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import{BrowserRouter, Routes, Route} from 'react-router-dom'
 import Signup from './pages/Signup'
 import Login from './pages/Login'
